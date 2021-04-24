5e94b1b1331478312f91ccad29abd5b31a08f05f
michelbbraga <michelbbraga@gmail.com>
2021-04-24 13:23:16 -0300

    Tratando consulta referente a Receita na classe ConsultDB

--- ControleDespesa4/ConsultDB.cs
+++ ControleDespesa4/ConsultDB.cs
@@ -208,5 +208,169 @@ namespace ControleDespesa4
 			}
 			return listadesp;
 		}
+		public List<string> ConsultaMovReceita()
+		{
+			var Sql = ("Select * from Mov_Receita");
+			var dataresult = db.ConsultaSQL(Sql);
+			try
+			{
+				for (int i = 0; i <= dataresult.Rows.Count - 1; i++)
+				{
+					string conteudo = dataresult.Rows[i]["Idmovreceita"].ToString() + " - " + dataresult.Rows[i]["Id_mv_receita"].ToString() + " - " + dataresult.Rows[i]["data_mov_receita"].ToString() + " - " + dataresult.Rows[i]["valor_rec"].ToString() + " - " + dataresult.Rows[i]["desc_receita"].ToString();
+					listadesp.Add(conteudo);
+					Console.WriteLine(conteudo);
+					msglog = " #BDC24 MOdulo de Consulta - Inciado com sucesso 24 ";
+				}
+				return listadesp;
+			}
+			catch
+			{
+				Console.WriteLine();
+				msglog = " #BDC24 MOdulo de Consulta - Inciado com sucesso 24 ";
+			}
+			return listadesp;
+		}
+		public List<string> ConsultaMovReceita(DateTime dtreceita)
+		{
+			var Sql = ($"select * from Mov_Receita where data_mov_receita = '{dtreceita}'");
+			var dataresult = db.ConsultaSQL(Sql);
+			try
+			{
+				for (int i = 0; i <= dataresult.Rows.Count - 1; i++)
+				{
+					string conteudo = dataresult.Rows[i]["Idmovreceita"].ToString() + " - " + dataresult.Rows[i]["Id_mv_receita"].ToString() + " - " + dataresult.Rows[i]["data_mov_receita"].ToString() + " - " + dataresult.Rows[i]["valor_rec"].ToString() + " - " + dataresult.Rows[i]["desc_receita"].ToString();
+					listadesp.Add(conteudo);
+					Console.WriteLine(conteudo);
+					msglog = " #BDC24 MOdulo de Consulta - Inciado com sucesso 24 ";
+				}
+				return listadesp;
+			}
+			catch
+			{
+				Console.WriteLine("Erro ao fazer a Consulta 25");
+				msglog = " #BDC20 MOdulo de Consulta - Erro despesa 25 ";
+			}
+			return listadesp;
+		}
+
+		public List<string> ConsultaMovReceita(int tipreceita)
+		{
+			var Sql = ($"select * from Mov_Receita where Id_mv_receita = {tipreceita} ");
+			var dataresult = db.ConsultaSQL(Sql);
+
+			try
+			{
+				for (int i = 0; i <= dataresult.Rows.Count - 1; i++)
+				{
+					string conteudo = dataresult.Rows[i]["Idmovreceita"].ToString() + " - " + dataresult.Rows[i]["Id_mv_receita"].ToString() + " - " + dataresult.Rows[i]["data_mov_receita"].ToString() + " - " + dataresult.Rows[i]["valor_rec"].ToString() + " - " + dataresult.Rows[i]["desc_receita"].ToString();
+					listadesp.Add(conteudo);
+					Console.WriteLine(conteudo);
+					msglog = " #BDC26 MOdulo de Consulta - Inciado com sucesso 26 ";
+				}
+				return listadesp;
+			}
+
+			catch
+			{
+				Console.WriteLine("Erro ao fazer a Conslulta 27");
+				msglog = " #BDC27 MOdulo de Inclusão - Erro despesa 27";
+			}
+			return listadesp;
+		}
+		public List<string> ConsultaMovReceita(string nomereceita)
+		{
+			var Sql = ($"select * from Mov_Receita where desc_receita like '%{nomereceita}%'");
+			var dataresult = db.ConsultaSQL(Sql);
+
+			try
+			{
+				for (int i = 0; i <= dataresult.Rows.Count - 1; i++)
+				{
+					string conteudo = dataresult.Rows[i]["Idmovreceita"].ToString() + " - " + dataresult.Rows[i]["Id_mv_receita"].ToString() + " - " + dataresult.Rows[i]["data_mov_receita"].ToString() + " - " + dataresult.Rows[i]["valor_rec"].ToString() + " - " + dataresult.Rows[i]["desc_receita"].ToString();
+					listadesp.Add(conteudo);
+					Console.WriteLine(conteudo);
+					msglog = " #BDC26 MOdulo de INclusão - Inciado com sucesso 26 ";
+				}
+				return listadesp;
+			}
+
+			catch
+			{
+				Console.WriteLine("Erro ao fazer a Inclusão 27");
+				msglog = " #BDC27 MOdulo de Inclusão - Erro despesa 27";
+			}
+			return listadesp;
+		}
+		public List<string> ConsultaReceita()
+		{
+			var Sql = ("Select * from Receita");
+			var dataresult = db.ConsultaSQL(Sql);
+			try
+			{
+				for (int i = 0; i <= dataresult.Rows.Count - 1; i++)
+				{
+					string conteudo = dataresult.Rows[i]["Idreceita"].ToString() + " - " + dataresult.Rows[i]["nomereceta"].ToString();
+					listadesp.Add(conteudo);
+					Console.WriteLine(conteudo);
+					msglog = " #BDC28 MOdulo de INclusão - Inciado com sucesso 28 ";
+				}
+				return listadesp;
+			}
+			catch
+			{
+				Console.WriteLine();
+				msglog = " #BDC28 MOdulo de INclusão - Inciado com sucesso 28 ";
+			}
+			return listadesp;
+		}
+		public List<string> ConsultaReceita(int receita)
+		{
+			var Sql = ($"select * from Receita where Idreceita = {receita}");
+			var dataresult = db.ConsultaSQL(Sql);
+			try
+			{
+				for (int i = 0; i <= dataresult.Rows.Count - 1; i++)
+				{
+					string conteudo = dataresult.Rows[i]["Idreceita"].ToString() + " - " + dataresult.Rows[i]["nomereceta"].ToString();
+					listadesp.Add(conteudo);
+					Console.WriteLine(conteudo);
+					msglog = " #BDC28 MOdulo de INclusão - Inciado com sucesso 28 ";
+
+				}
+				return listadesp;
+			}
+
+			catch
+			{
+				Console.WriteLine("Erro ao fazer a Inclusão 27");
+				msglog = " #BDC27 MOdulo de Inclusão - Erro despesa 27";
+			}
+			return listadesp;
+
+		}
+		public List<string> ConsultaReceita(string receita)
+		{
+			var Sql = ($"select * from receita where nomereceta like '%{receita}%'");
+			var dataresult = db.ConsultaSQL(Sql);
+			try
+			{
+				for (int i = 0; i <= dataresult.Rows.Count - 1; i++)
+				{
+					string conteudo = dataresult.Rows[i]["Idreceita"].ToString() + " - " + dataresult.Rows[i]["nomereceta"].ToString();
+					listadesp.Add(conteudo);
+					Console.WriteLine(conteudo);
+					msglog = " #BDC28 MOdulo de INclusão - Inciado com sucesso 28 ";
+
+				}
+				return listadesp;
+			}
+
+			catch
+			{
+				Console.WriteLine("Erro ao fazer a Inclusão 29");
+				msglog = " #BDC29 MOdulo de Inclusão - Erro despesa 29";
+			}
+			return listadesp;
+		}
 	}
 }
